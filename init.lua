38b955912a3a2530bdeae7c2597ca4529e2d3bae
Dave Rogers <dave@drogers.us>
2021-07-24 15:34:20 -0400

    add window switcher

--- init.lua
+++ init.lua
@@ -433,3 +433,20 @@ function winkey:exited()
 --              "Leaving window operations mode", "")
 end
 
+-- try hs.window.switcher
+-- set up your windowfilter
+switcher = hs.window.switcher.new() -- default windowfilter: only visible windows, all Spaces
+switcher_space = hs.window.switcher.new(hs.window.filter.new():setCurrentSpace(true):setDefaultFilter{}) -- include minimized/hidden windows, current Space only
+--switcher_browsers = hs.window.switcher.new{'Safari','Google Chrome'} -- specialized switcher for your dozens of browser windows :)
+
+-- bind to hotkeys; WARNING: at least one modifier key is required!
+hs.hotkey.bind('cmd-alt','tab','Next window',function()switcher_space:next()end)
+hs.hotkey.bind('cmd-alt-shift','tab','Prev window',function()switcher_space:previous()end)
+hs.hotkey.bind('cmd-alt','right','Next window',function()switcher_space:next()end)
+hs.hotkey.bind('cmd-alt','left','Prev window',function()switcher_space:previous()end)
+
+
+-- alternatively, call .nextWindow() or .previousWindow() directly (same as hs.window.switcher.new():next())
+--hs.hotkey.bind('alt','tab','Next window',hs.window.switcher.nextWindow)
+-- you can also bind to `repeatFn` for faster traversing
+--hs.hotkey.bind('alt-shift','tab','Prev window',hs.window.switcher.previousWindow,nil,hs.window.switcher.previousWindow)
